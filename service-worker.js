f6d44e82f5ba88abf13a0f4abe626598b44f7985
4l3b <alessandro.bigolin.fdi@gmail.com>
2025-03-05 12:42:18 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,14 @@
 self.addEventListener('install', (event) => {
+  // Durante l'installazione, memorizziamo tutte le risorse necessarie nella cache
   event.waitUntil(
     caches.open('my-cache').then((cache) => {
       return cache.addAll([
         '/workTimeCalculator_iOS/index.html',
         '/workTimeCalculator_iOS/manifest.json',
         '/workTimeCalculator_iOS/alarm.mp3',
-        '/workTimeCalculator_iOS/offline.html'  // Aggiungi la pagina offline alla cache
+        '/workTimeCalculator_iOS/icons/icon-180x180.png',
+        '/workTimeCalculator_iOS/icons/icon-192x192.png',
+        '/workTimeCalculator_iOS/icons/icon-512x512.png',
       ]);
     })
   );
@@ -14,16 +17,17 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      // Se la risorsa è nella cache, restituirla
+      // Se la risorsa è nella cache, restituiscila
       if (cachedResponse) {
         return cachedResponse;
       }
 
       // Se non è nella cache, prova a fare una richiesta di rete
       return fetch(event.request).catch(() => {
-        // Se non c'è connessione, restituisci la pagina offline dalla cache
-        return caches.match('/workTimeCalculator_iOS/offline.html');
+        // Se offline, ritorna la risorsa dalla cache (fallback a index.html)
+        return caches.match('/workTimeCalculator_iOS/index.html');
       });
     })
   );
 });
+
