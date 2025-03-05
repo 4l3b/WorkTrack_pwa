979b6a24fa83addb1fe1e91035b6b5b9d2847ef5
4l3b <alessandro.bigolin.fdi@gmail.com>
2025-03-05 12:35:57 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,8 @@ self.addEventListener('install', (event) => {
       return cache.addAll([
         '/workTimeCalculator_iOS/index.html',
         '/workTimeCalculator_iOS/manifest.json',
-        '/workTimeCalculator_iOS/alarm.mp3'
+        '/workTimeCalculator_iOS/alarm.mp3',
+        '/workTimeCalculator_iOS/offline.html'  // Aggiungi la pagina offline alla cache
       ]);
     })
   );
@@ -13,7 +14,16 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
+      // Se la risorsa è nella cache, restituirla
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // Se non è nella cache, prova a fare una richiesta di rete
+      return fetch(event.request).catch(() => {
+        // Se non c'è connessione, restituisci la pagina offline dalla cache
+        return caches.match('/workTimeCalculator_iOS/offline.html');
+      });
     })
   );
 });
